e6615dad86d0b799a620817a76cb210f6eba65e9
Pete Peterson <petersonpf@ornl.gov>
2015-12-15 11:53:04 -0500

    Scripts for downloading number of downloads

---
+++ downloads_github.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl  https://api.github.com/repos/mantidproject/mantid/releases -o github.json
---
+++ downloads_sourceforge.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+curl -L 'https://sourceforge.net/projects/mantid/files/2.3/stats/json?start_date=2012-01-01&end_date=2015-12-31' -o sourceforge-2.3.json
+curl -L 'https://sourceforge.net/projects/mantid/files/2.4/stats/json?start_date=2012-01-01&end_date=2015-12-31' -o sourceforge-2.4.json
+curl -L 'https://sourceforge.net/projects/mantid/files/2.5/stats/json?start_date=2012-01-01&end_date=2015-12-31' -o sourceforge-2.5.json
+curl -L 'https://sourceforge.net/projects/mantid/files/2.6/stats/json?start_date=2012-01-01&end_date=2015-12-31' -o sourceforge-2.6.json
+curl -L 'https://sourceforge.net/projects/mantid/files/3.0/stats/json?start_date=2012-01-01&end_date=2015-12-31' -o sourceforge-3.0.json
+curl -L 'https://sourceforge.net/projects/mantid/files/3.1/stats/json?start_date=2012-01-01&end_date=2015-12-31' -o sourceforge-3.1.json
+curl -L 'https://sourceforge.net/projects/mantid/files/3.2/stats/json?start_date=2012-01-01&end_date=2015-12-31' -o sourceforge-3.2.json
+curl -L 'https://sourceforge.net/projects/mantid/files/3.3/stats/json?start_date=2012-01-01&end_date=2015-12-31' -o sourceforge-3.3.json
+curl -L 'https://sourceforge.net/projects/mantid/files/3.4/stats/json?start_date=2012-01-01&end_date=2015-12-31' -o sourceforge-3.4.json
+curl -L 'https://sourceforge.net/projects/mantid/files/3.5/stats/json?start_date=2012-01-01&end_date=2015-12-31' -o sourceforge-3.5.json
